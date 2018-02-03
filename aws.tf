1f0a3f9ca111f5fcc396f1d832064b4d9b5498cb
Joe Rodriguez <joerod@ad.joerod.com>
2018-02-03 18:52:25 -0500

    first commit

---
+++ aws.tf
@@ -0,0 +1,39 @@
+provider "aws" {
+  access_key = "${var.aws_access_key}"
+  secret_key = "${var.aws_secret_key}"
+  region     = "${var.aws_region}"
+}
+
+resource "aws_security_group" "ssh" {
+  name = "Allow SSH"
+  description = "Managed by JoeRod"
+  ingress {
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags {
+    Name = "ssh"
+  }
+}
+
+resource "aws_instance" "joerod" {
+    ami           = "ami-b374d5a5"
+    instance_type = "t2.micro"
+    key_name = "${aws_key_pair.deployer.key_name}"
+    vpc_security_group_ids = ["${aws_security_group.ssh.id}"]
+    provisioner "local-exec" {
+    command = "echo ${aws_instance.joerod.public_ip} > ip_address.txt"
+   
+  }
+   tags = {
+       Name  = "${var.aws_resource_name}"
+     }
+
+}
+
+resource "aws_eip" "ip" {
+  instance = "${aws_instance.joerod.id}"
+}
