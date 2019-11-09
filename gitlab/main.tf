e7a921354e7210f5d7611e8a11f60d90e5b10346
Joe Rodriguez <joerod@gmail.com>
2019-11-08 23:58:55 -0500

    imported project

--- gitlab/main.tf
+++ gitlab/main.tf
@@ -3,6 +3,7 @@ provider "gitlab" {
   version = "~> 2.3"
 }
 
+
 resource "gitlab_project" "sample_project" {
   name = "${var.project_name}"
   approvals_before_merge  = 1
@@ -10,6 +11,12 @@ resource "gitlab_project" "sample_project" {
   issues_enabled = true
   visibility_level = "public"
   initialize_with_readme = true
-
 }
-
+resource "gitlab_project" "sample_project2" {
+  name = "Test"
+  approvals_before_merge  = 1
+  only_allow_merge_if_all_discussions_are_resolved = true
+  issues_enabled = true
+  visibility_level = "public"
+  initialize_with_readme = true
+}
